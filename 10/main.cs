0ca4fedf7fdb5de4e384c8ac617e8aef585f22a2
Edwin Viña R <111668890+ecodant@users.noreply.github.com>
2022-11-09 09:17:34 -0600

    Update main.cs

--- 10/main.cs
+++ 10/main.cs
@@ -3,22 +3,34 @@ using System.Text.RegularExpressions;
 class HelloWorld {
   static void Main() {
     string alumno;
+    bool noteConfirm = false;
     int nota, count = 0;
     do
     {
-        Console.WriteLine("Ingrese el nombre y apellido del usuario del alumno");
+        Console.WriteLine("Ingrese el nombre y apellido del alumno");
         alumno = Console.ReadLine();
         Regex reg = new Regex(@"[a-zA-Z] ");
         if (reg.IsMatch(alumno) == true)
         {
-            Console.WriteLine("Ingrese la nota del alumno");
-            nota = int.Parse(Console.ReadLine());
-            count++;
-            Console.WriteLine("El alumno: "+ alumno +" tiene una nota de " + nota);
+            while (noteConfirm == false)
+            {
+                Console.WriteLine("Ingrese la nota del alumno");
+                if (int.TryParse(Console.ReadLine(), out nota) == true)
+                {
+                    Console.WriteLine("El alumno: "+ alumno +" tiene una nota de " + nota);
+                    count++;
+                    noteConfirm = true;
+                }
+                else 
+                {
+                    Console.WriteLine("***Error!, la nota debe ser un número entero");
+                }
+            }
+            noteConfirm = false;
         }
         else
         {
-            Console.WriteLine("***Porfavor escriba un nombre y apellido");
+            Console.WriteLine("***Error!, el sistema requiere un nombre y apellido");
         }
     }
     while(count < 24);
